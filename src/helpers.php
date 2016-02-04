889d003e7168a2c203f56540549ef98064deee7c
Renato Dehnhardt <renato.dehnhardt@rentcars.com>
2016-02-04 16:55:07 -0200

    First Commit

--- src/helpers.php
+++ src/helpers.php
@@ -7,7 +7,7 @@ if (!function_exists('__')) {
     function __($key)
     {
         $client = new Client(['base_uri' => env('TRANSLATE_URL')]);
-        $translate = $client->get('translate', ['query' => ['key' => $key, 'locale' => App::getLocale()]]);
+        $translate = $client->get('api/translate', ['query' => ['key' => $key, 'locale' => App::getLocale()]]);
 
         if ($translate->getStatusCode() == 200) {
             $message = json_decode($translate->getBody()->getContents());
@@ -23,7 +23,7 @@ if (!function_exists('getLocales')) {
     function getLocales()
     {
         $client = new Client(['base_uri' => env('TRANSLATE_URL')]);
-        $translate = $client->get('locales');
+        $translate = $client->get('api/locales');
 
         if ($translate->getStatusCode() == 200) {
             return json_decode($translate->getBody()->getContents());
